65905c61e88b4a3cf4525c42f68da58bfd72781d
hwangsangg <106916150+hwangsangg@users.noreply.github.com>
2022-06-05 20:22:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Phượng à, 5 hên đây :>',
+    introTitle: 'Gửi Vy',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: '8 thích anh <33',
-    btnNo: 'Không, 8 coi 5 là a trai :3',
+    btnYes: 'Em Yêu Anh <3',
+    btnNo: 'Tao Yêu Mày <3',
     question:'Trên thế giới hơn 7 tỉ người mà sao 8 lại yêu 5 <3',
-    btnReply: 'Gửi cho lão 5 hên <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu 8 à <33333333',
-    mess: '5 biết mà 🥰. Yêu 8 nhiều nhiều 😘😘',
-    messDesc: 'Nhắn tin cho tôi nhe 8 :>>.',
+    btnReply: 'Yêu Mày <3',
+    reply: '<3',
+    mess: '<3',
+    messDesc: 'inbox tao i',
     btnAccept: 'Âu kây :3',
-    messLink: 'https://m.me/vvnuc' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/hwangsangg/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
